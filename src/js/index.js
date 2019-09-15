0fcd5e4bccec837bc605ea3e7054ec35ad9656eb
E!-XP <poczta.elektroniczna@hotmail.com>
2019-09-15 22:15:55 +0200

    update mobile menu and footer

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,11 @@ const navList = document.getElementById('nav-list');
 const navButton = document.getElementById('nav-btn');
 
 const isNavOpen = () => mainNav.classList.contains('js-is-open');
-const blockScroll = () => window.scrollTo(0, 0);
-const enableTransition = () => navList.classList.remove('js-transition-none');
+const toggleScrollBlock = () =>
+  document.body.parentElement.classList.toggle('h-block-page-scroll');
+const enableTransition = () => navList.classList.remove('h-transition-none');
 const disableTransition = () => {
-  if (!isNavOpen()) navList.classList.add('js-transition-none');
+  if (!isNavOpen()) navList.classList.add('h-transition-none');
 };
 
 const onNavClick = () => {
@@ -14,8 +15,7 @@ const onNavClick = () => {
   if (isNavOpen()) enableTransition();
   navButton.setAttribute('aria-expanded', `${isNavOpen()}`);
 
-  const methods = ['removeEventListener', 'addEventListener'];
-  window[methods[+isNavOpen()]]('scroll', blockScroll);
+  toggleScrollBlock();
 };
 
 navButton.addEventListener('click', onNavClick);
